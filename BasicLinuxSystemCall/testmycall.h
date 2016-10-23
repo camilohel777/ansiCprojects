31d5903776654392dcccc3f3698cf6cb5b27d50f
Camilo Enrique Rivera <camilo.rive007@gmail.com>
2016-10-23 11:32:14 -0400

    Add files via upload

---
+++ BasicLinuxSystemCall/testmycall.h
@@ -0,0 +1,11 @@
+
+/*---Start of header file ----- */
+
+#include <linux/unistd.h>
+#define __NR_mycall 341
+
+extern long int syscall(long int__sysno, ...)__THROW;
+
+/*---End of header file-------*/
+
+
